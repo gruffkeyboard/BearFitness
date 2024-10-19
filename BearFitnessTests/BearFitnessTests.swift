d615eb76a84977674d17ea6e44c42481da00b636
Shakthi Gnana <sgna2711@gmail.com>
2024-10-19 15:48:06 -0500

    Initial Commit

---
+++ BearFitnessTests/BearFitnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  BearFitnessTests.swift
+//  BearFitnessTests
+//
+//  Created by Shakthivel Gnanasekaran on 10/19/24.
+//
+
+import Testing
+@testable import BearFitness
+
+struct BearFitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BearFitnessUITests/BearFitnessUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BearFitnessUITestsLaunchTests.swift
+//  BearFitnessUITests
+//
+//  Created by Shakthivel Gnanasekaran on 10/19/24.
+//
+
+import XCTest
+
+final class BearFitnessUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
