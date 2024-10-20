d7f9983a5f17d8ca8564462becabaa3c3d7d48fe
Shakthi Gnana <sgna2711@gmail.com>
2024-10-20 00:55:00 -0500

    Second Commit

---
+++ BearFitness/Authentication/SignInEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInEmailView.swift
+//  BearFitness
+//
+//  Created by Shakthivel Gnanasekaran on 10/19/24.
+//
+
+import SwiftUI
+
+struct SignInEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInEmailView()
+}
---
+++ BearFitness/Utilities/Utilities.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  BearFitness
+//
+//  Created by Shakthivel Gnanasekaran on 10/20/24.
+//
+
+import Foundation
+
+final class Utilities {
+    
+    static let shared = Utilities()
+    
+    private init() {
+        
+        
+    }
+}
